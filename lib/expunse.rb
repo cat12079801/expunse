8200993ad6f7844236f51c3b6ca7fad373ea6edf
cat12079801 <cat12079801@gmail.com>
2016-01-26 00:24:08 +0900

    とりあえずnattoでmecabつかって形態素分析

--- lib/expunse.rb
+++ lib/expunse.rb
@@ -1,5 +1,13 @@
 require "expunse/version"
+require "natto"
 
 module Expunse
-  # Your code goes here...
+  class << self
+    def judge str
+      natto = Natto::MeCab.new
+      natto.parse(str.to_s) do |s|
+        puts "#{s.surface}\t#{s.feature}"
+      end
+    end
+  end
 end
